3f0f331698667e9c1b9ba6249e755705027630b9
Asman Mamou <114336577+JolyBI3ll@users.noreply.github.com>
2025-05-22 00:02:31 +0300

    Delete taiga-contrib-drawio/taiga_front_drawio directory

--- taiga-contrib-drawio/taiga_front_drawio/index.ts
+++
@@ -1,6 +0,0 @@
-import { TaigaApp } from '@taiga-ui/core';
-import { DrawioModule } from './drawio.module';
-
-export function activate(app: TaigaApp) {
-  app.attachments.registerExtraAction(DrawioModule);
-}
